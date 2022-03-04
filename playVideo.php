81cb84dd9eff67435e52f134438c7b95b7f6e639
Md. Zobayer Ibna Kabir <66842328+ZobayerAkib@users.noreply.github.com>
2022-03-05 00:30:56 +0600

    Add card,play videos,upload videos,create category,contact us page,admin panel,check messages

--- playVideo.php
+++ playVideo.php
@@ -1,86 +1,82 @@
 <?php
-include 'connect.php';
-include 'nav.php';
-if (!isset($_SESSION['user_id'])) {
-    echo "<script>alert('Please log in to view this page!');location.href='login.php';</script>";
-}
+    include "nav.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View</title>
-    <style>
-        .Videos{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-wrap: wrap;
-           
-        }
-
-        video {
-            width: 640px;
-            height: 260px;
-            padding-top: 10px;
-
-        }
-
-      .link {
-            text-decoration: none;
-            font-size: 1.5rem;
-            color:red;
-            padding-left:600px;
-
-               
-        }
-     
-    </style>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/playVideo.css">
 </head>
 
 <body>
-    <section >
+    <div class="container mt-5">
+        <div class="header text-center mt-5">
+            <h2>
+                All Video
+                <small class="text-muted">Tutorials </small>
+            </h2>
+            <h3 class="text-muted mt-5">
+            “Education is the most powerful weapon which you can use to change the world” 
+           <br> – Nelson Mandela</br>
+            </h3>
+        </div>
+        <div style="margin-top:100px ;" class="row">
+            <?php
 
-    
-    <div class="upload">
-        <?php
-        if ($_SESSION['user_name'] == 'admin') {
-        ?>
-            <a  class="link" href="uploadVid.php">UPLOAD</a>
-        <?php
-        }
-        ?>
-    </div>
+            include "connect.php";
 
-    <div class="alb mt-5">
 
-        <?php
-        $sql = "SELECT * FROM videos ORDER BY id DESC";
-        $res = mysqli_query($conn, $sql);
+            $name = $_GET['p_name'];
 
-        if (mysqli_num_rows($res) > 0) {
-            while ($video = mysqli_fetch_assoc($res)) {
-        ?>
 
-               
+            $sql = "SELECT * FROM video_details where name = '$name' ";
+            $res = mysqli_query($conn, $sql);
 
-                <video controls="controls" source src="uploads/<?= $video['video_url'] ?>" >
-                </video>
+            if (mysqli_num_rows($res) > 0) {
+                while ($video = mysqli_fetch_assoc($res)) {
+            ?>
 
-        <?php
+                    <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+
+                        <form action="" method="GET">
+
+                            <div class="card" style="width: 20rem;">
+                                <video controls="controls" source src="uploads/<?= $video['video_url'] ?>" type="video/mp4">
+                                </video>
+                                <div class="card-body mt-5">
+                                    <h5 class="card-title" style="color: tomato;font-weight:bold"><?PHP echo $video['name'] ?></h5>
+                                    <p class="card-text text-center"><?PHP echo $video['Description'] ?></p>
+
+
+                                </div>
+                            </div>
+
+
+                        </form>
+
+                    </div>
+
+
+
+
+
+            <?php
+                }
             }
-        } else {
-            echo "<h1>Empty</h1>";
-        }
-        ?>
+
+
+            ?>
+        </div>
     </div>
-    </section>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
+
 
 </html>
